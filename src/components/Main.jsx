e3762d1184f6c82c5dcd17d903dcef467257b7e4
Hariharasudhan M <harimhs028@gmail.com>
2024-12-19 23:38:57 +0530

    Form handling in main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Main = () => {
+
+    const [meme, setMeme] = useState({
+        imageUrl: "http://i.imgflip.com/1bij.jpg",
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+    });
+
+    function handleChange(event){
+        const {value, name} = event.currentTarget;
+        setMeme(prev=>({
+            ...prev,
+            [name]: value,
+        }))
+    }
   return (
-    <div>Main</div>
+    <main>
+    <div className='form'>
+        <label>Top Text:
+            <input 
+            type='text'
+            placeholder=''
+            name='topText'
+            onChange={handleChange}
+            value={meme.topText}
+            />
+        </label>
+        <label>Bottom Text:
+            <input 
+            type='text'
+            placeholder=''
+            name='bottomText'
+            onChange={handleChange}
+            value={meme.bottomText}
+            />
+        </label>
+        <button>Get the meme image!</button>
+    </div>
+    <div className="meme">
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+    </div>
+    </main>
   )
 }
 
