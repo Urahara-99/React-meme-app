1aadaf633c8a2cd429536f2e2ed1d954a1f4fdc1
Hariharasudhan M <harimhs028@gmail.com>
2024-12-21 18:09:57 +0530

    Added windowTracker task

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import React from "react"
 import FetchData from "./components/FetchData"
 import Header from "./components/Header"
 import Main from "./components/Main"
+import WindowTracker from "./components/WindowTracker"
 
 
 function App() {
-  
+
+  const [show, setShow] = React.useState(true);
+  function handleToggle() {
+    setShow(prev=> !prev)
+  }
   return (
     <>
-      <Header/>
-      <Main />
+      {/* <Header/>
+      <Main /> */}
       {/* <FetchData /> */}
+      <main className="container">
+            <button onClick={handleToggle}>
+                Toggle WindowTracker
+            </button>
+            {show && < WindowTracker />}
+        </main>
     </>
   )
 }
